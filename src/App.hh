a149608e168e7af857330c4a7940a4bc99ca782b
korei999 <ju7t1xe@gmail.com>
2024-09-02 02:40:50 +0300

    c -> s

--- src/App.hh
+++ src/App.hh
@@ -40,19 +40,19 @@ struct App
     bool bFullscreen = false;
 };
 
-ADT_NO_UB constexpr void AppInit(App* c) { c->pVTable->init(c); }
-ADT_NO_UB constexpr void AppDisableRelativeMode(App* c) { c->pVTable->disableRelativeMode(c); }
-ADT_NO_UB constexpr void AppEnableRelativeMode(App* c) { c->pVTable->enableRelativeMode(c); }
-ADT_NO_UB constexpr void AppTogglePointerRelativeMode(App* c) { c->pVTable->togglePointerRelativeMode(c); }
-ADT_NO_UB constexpr void AppToggleFullscreen(App* c) { c->pVTable->toggleFullscreen(c); }
-ADT_NO_UB constexpr void AppSetCursorImage(App* c, String cursorType) { c->pVTable->setCursorImage(c, cursorType); }
-ADT_NO_UB constexpr void AppSetFullscreen(App* c) { c->pVTable->setFullscreen(c); }
-ADT_NO_UB constexpr void AppUnsetFullscreen(App* c) { c->pVTable->unsetFullscreen(c); }
-ADT_NO_UB constexpr void AppBindGlContext(App* c) { c->pVTable->bindGlContext(c); }
-ADT_NO_UB constexpr void AppUnbindGlContext(App* c) { c->pVTable->unbindGlContext(c); }
+ADT_NO_UB constexpr void AppInit(App* s) { s->pVTable->init(s); }
+ADT_NO_UB constexpr void AppDisableRelativeMode(App* s) { s->pVTable->disableRelativeMode(s); }
+ADT_NO_UB constexpr void AppEnableRelativeMode(App* s) { s->pVTable->enableRelativeMode(s); }
+ADT_NO_UB constexpr void AppTogglePointerRelativeMode(App* s) { s->pVTable->togglePointerRelativeMode(s); }
+ADT_NO_UB constexpr void AppToggleFullscreen(App* s) { s->pVTable->toggleFullscreen(s); }
+ADT_NO_UB constexpr void AppSetCursorImage(App* s, String cursorType) { s->pVTable->setCursorImage(s, cursorType); }
+ADT_NO_UB constexpr void AppSetFullscreen(App* s) { s->pVTable->setFullscreen(s); }
+ADT_NO_UB constexpr void AppUnsetFullscreen(App* s) { s->pVTable->unsetFullscreen(s); }
+ADT_NO_UB constexpr void AppBindGlContext(App* s) { s->pVTable->bindGlContext(s); }
+ADT_NO_UB constexpr void AppUnbindGlContext(App* s) { s->pVTable->unbindGlContext(s); }
 ADT_NO_UB constexpr void AppSetSwapInterval(App* c, int interval) { c->pVTable->setSwapInterval(c, interval); }
-ADT_NO_UB constexpr void AppToggleVSync(App* c) { c->pVTable->toggleVSync(c); }
-ADT_NO_UB constexpr void AppSwapBuffers(App* c) { c->pVTable->swapBuffers(c); }
-ADT_NO_UB constexpr void AppProcEvents(App* c) { c->pVTable->procEvents(c); }
-ADT_NO_UB constexpr void AppShowWindow(App* c) { c->pVTable->showWindow(c); }
-ADT_NO_UB constexpr void AppDestroy(App* c) { c->pVTable->destroy(c); }
+ADT_NO_UB constexpr void AppToggleVSync(App* s) { s->pVTable->toggleVSync(s); }
+ADT_NO_UB constexpr void AppSwapBuffers(App* s) { s->pVTable->swapBuffers(s); }
+ADT_NO_UB constexpr void AppProcEvents(App* s) { s->pVTable->procEvents(s); }
+ADT_NO_UB constexpr void AppShowWindow(App* s) { s->pVTable->showWindow(s); }
+ADT_NO_UB constexpr void AppDestroy(App* s) { s->pVTable->destroy(s); }
